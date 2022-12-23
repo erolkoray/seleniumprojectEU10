6e3148c7d8ed05c94e587b16ab264de3f960f63d
Koray <koray_4404@hotmail.com>
2022-12-23 22:47:34 +0000

    SeleniumTest and BasicNavigations created

---
+++ src/test/java/com/cydeo/tests/day1_selenium_intro/BasicNavigations.java
@@ -0,0 +1,53 @@
+package com.cydeo.tests.day1_selenium_intro;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BasicNavigations {
+
+    public static void main(String[] args) throws InterruptedException {
+        //1- Set up the browser driver
+        WebDriverManager.chromedriver().setup();
+
+        //2- Create instance of the Selenium WebDriver
+        WebDriver driver = new ChromeDriver(); // this line will open an empty browser for us
+
+        //3- Go to https://www.tesla.com
+        driver.get("https://www.tesla.com");
+
+        //4- Delay the code execution for 1 seconds
+        Thread.sleep(1000);
+
+        //5- use selenium to navigate back
+        driver.navigate().back();
+
+        //6- use selenium to navigate forward & Delay the code execution for 1 seconds
+        Thread.sleep(1000);
+        driver.navigate().forward();
+        String currentTitle = driver.getTitle(); // gets the current title
+        System.out.println("Website title: "+ currentTitle);
+        String currentURL = driver.getCurrentUrl();
+        System.out.println("Current URL: "+currentURL);
+
+        // Delay the code execution for 1 second and go to amazon.com then go to amazon prime
+        Thread.sleep(1000);
+        driver.get("https://www.amazon.com");
+        driver.navigate().to("https://www.amazon.com/prime");
+
+        // Delay the code execution for 1 seconds & refresh the page
+        Thread.sleep(1000);
+        driver.navigate().refresh();
+
+        // get the title of the page
+        currentTitle = driver.getTitle(); // gets the latest website title after re-assigning the getTitle to CurrentTitle variable
+        System.out.println("Website title: "+ currentTitle);
+
+        currentURL = driver.getCurrentUrl();
+        System.out.println("Current URL: "+currentURL);
+
+
+    }
+
+
+}
