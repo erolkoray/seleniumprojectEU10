d7500a27724b81d4270de80f5a1eb7650b7e6f0b
Koray <koray_4404@hotmail.com>
2023-01-07 22:59:52 +0000

    Selenium Automation day 5 - TestNG_Selenium.java updated. T4_SimpleDropdowns.java created

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Selenium.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Selenium.java
@@ -3,23 +3,35 @@ package com.cydeo.tests.day5_testNG_intro_dropdowns;
 import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
 public class TestNG_Selenium {
 
-    @Test
-    public void selenium_test(){
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod(){      // EVERYTHING IN THIS METHOD NOW WILL AUTOMATICALLY RUN BEFORE EVERY OTHER METHOD (EG. TEST METHODS)
         //Do Browser driver setup
         //Open browser
-        WebDriver driver = WebDriverFactory.getDriver("chrome");
-
+        driver = WebDriverFactory.getDriver("chrome");
         //maximise the page
         driver.manage().window().maximize();
-
         //implicit wait
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+    }
+
+    @AfterMethod
+    public void tearDownMethod(){ //EVERYTHING IN THIS METHOD WILL AUTOMATICALLY RUN AFTER EVERY OTHER TEST METHOD
+
+        driver.close();
+    }
+
+    @Test
+    public void selenium_test(){
 
         //get"https://google.com"
         driver.get("https://google.com");
@@ -28,8 +40,6 @@ public class TestNG_Selenium {
         String actualTitle = driver.getTitle();
         String expectedTitle = "Google";
         Assert.assertEquals(actualTitle, expectedTitle, "Title is not matching here."); // message will only show if the test fails.
-
-        driver.close();
 
     }
 
