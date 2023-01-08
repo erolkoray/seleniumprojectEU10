3770ccadc131030777b87db11203416fae043f6a
Koray <koray_4404@hotmail.com>
2023-01-08 14:42:15 +0000

    Automation day 6 - T4 iframe task updated

--- src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T4_Iframes.java
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T4_Iframes.java
@@ -27,6 +27,8 @@ public class T4_Iframes {
         driver.get("https://practice.cydeo.com/iframe  ");
     }
 
+
+//  @ignore - means it will skip the following test method
     @Test
     public void iframe_test1(){
 
@@ -34,18 +36,28 @@ public class T4_Iframes {
         // Opt. 1  - switching to iframe using id attribute value
         driver.switchTo().frame("mce_0_ifr");
 
+        //Opt 2
+     //  driver.switchTo().frame(0); - this is by the index number. index number is always whatever results it returns in the inspect HTML page -1
+
+        //Opt 3
+     //  driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@id='mce_0_ifr']")
+
         // locate the p tag
         WebElement yourContentGoesHereText = driver.findElement(By.xpath("//p"));
 
         Assert.assertTrue(yourContentGoesHereText.isDisplayed());
 
+        //verify "An iframe containing the TinyMCE WYSIWYG Editor"
+        // we need to switch back to ParentFrame from iframe
+        driver.switchTo().parentFrame();
+
+        WebElement headerText = driver.findElement(By.xpath("//h3"));
+
+        // Assertion of the header text is displayed or not
+
+        Assert.assertTrue(headerText.isDisplayed());
+
 
     }
 
 }
-/*
-TC #4: Iframe practice
-
-4. Assert: “Your content goes here.” Text is displayed.
-5. Assert: “An iFrame containing the TinyMCE WYSIWYG Editor”
- */
