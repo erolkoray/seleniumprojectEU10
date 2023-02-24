d6bd6fb2f173c75d3d34373f31f705831bba0d6b
Koray <koray_4404@hotmail.com>
2023-02-24 22:42:36 +0000

    Automation day 12 - POMPractices(Pom design practice) created- LibraryLoginPage.java updated.

--- src/test/java/com/cydeo/pages/LibraryLoginPage.java
+++ src/test/java/com/cydeo/pages/LibraryLoginPage.java
@@ -1,26 +1,40 @@
 package com.cydeo.pages;
 
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LibraryLoginPage {
 
+    //#1- initialize the driver instance and object of the class
     public LibraryLoginPage(){
-        PageFactory.initElements(Driver.getDriver(),this);
-
+        /*initElements method will create connection in between the current
+        driver session (instance) and the object of the current class.
+         */
+        PageFactory.initElements(Driver.getDriver(), this);
     }
 
 
+    //#2- use @FindBy annotation to locate web elements
 
-    //TC #1: Required field error message test
-    //1- Open a chrome browser
-    //2- Go to: https://library1.cydeo.com
-    //3- Do not enter any information
-    //4- Click to “Sign in” button
-    //5- Verify expected error is displayed:
-    //Expected: This field is required.
-    //
-    //NOTE: FOLLOW POM DESIGN PATTERN
+    @FindBy(xpath = "//input[@id='inputEmail']")
+    public WebElement  inputUsername;
+
+    @FindBy(id = "inputPassword")
+    public WebElement inputPassword;
+
+    @FindBy(xpath = "//button[.='Sign in']")
+    public WebElement signInButton;
+
+    @FindBy(xpath = "//div[.='This field is required.']/div")
+    public WebElement fieldRequiredErrorMessage;
+
+    @FindBy(xpath = "//div[.='Please enter a valid email address.']/div")
+    public WebElement enterValidEmailErrorMessage;
+
+    @FindBy(xpath = "//div[@class=\"mb-4\"]/div")
+    public WebElement wrongEmailOrPasswordErrorMessage;
 
 
 }
